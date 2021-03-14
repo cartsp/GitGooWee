77861683a951531549b950ddccacf0e536db0215
Liam Boyle <ltboyle@gmail.com>
2021-03-14 23:12:52 +0000

    Initial commit

---
+++ GitGooWee/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitGooWee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
