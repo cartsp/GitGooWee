b28e322e94df954ae1dac2f5acbf2e6e7ae72242
Liam Boyle <ltboyle@gmail.com>
2021-03-15 17:30:22 +0000

    Converted to linq expression

--- GitGooWee/GitRepo.cs
+++ GitGooWee/GitRepo.cs
@@ -12,23 +12,13 @@ namespace GitGooWee
             return ParseBranchString(res);
         }
 
-        private static IEnumerable<Branch> ParseBranchString(string branchString)
-        {
-            var branchStrings = branchString.Split('\n');
-
-            foreach(var branch in branchStrings.Skip(1).SkipLast(1))
-            {
-                if (branch.StartsWith("*"))
-                {
-                    yield return new Branch() { Name = branch.Substring(2), Current = true };
-                }
-                else
-                {
-                    yield return new Branch() { Name = branch };
-                }
-                
-            }
-        }
-
+        private static IEnumerable<Branch> ParseBranchString(string branchString) =>
+            branchString
+                .Split('\n')
+                .Skip(1)
+                .SkipLast(1)
+                .Select(branch => branch.StartsWith("*")
+                    ? new Branch() { Name = branch[2..], Current = true }
+                    : new Branch() { Name = branch });
     }
 }
